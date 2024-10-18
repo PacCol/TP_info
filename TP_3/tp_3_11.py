9f623136cbd277ea9f36eb6d0322fe7002afee51
--global <--global>
2024-10-18 11:50:03 +0200

    update

--- TP_3/tp_3_11.py
+++ TP_3/tp_3_11.py
@@ -3,18 +3,31 @@ import matplotlib.pyplot as plt
 col1 = []
 col2 = []
 
-f = open("Fichiers/tableau2.csv")
-content = f.read().split("\n")
-print(content)
-f.close()
+def lecture_donnee(chemin):
 
-for i in content:
     try:
-        col2.append(float(i.split(";")[1]))
-        col1.append(float(i.split(";")[0]))
+        if chemin.split(".")[-1] != "csv":
+            return
     except:
-        pass
+        return
 
-plt.figure("Graph")
-plt.plot(col1, col2)
-plt.show()
+    f = open(chemin, "r")
+    content = f.read().split("\n")
+    f.close()
+
+    for i in content:
+        if ";" in i:
+            try:
+                col1.append(float(i.split(";")[0]))
+                try:
+                    col2.append(float(i.split(";")[1]))
+                except:
+                    col2.append(0)
+            except:
+                pass
+
+    plt.figure("Graph")
+    plt.plot(col1, col2)
+    plt.show()
+
+lecture_donnee("Fichiers/tableau2.csv")
